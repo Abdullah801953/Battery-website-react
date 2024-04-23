9088ea783a7e6e6d9cdacde009db4c1dd16f94c2
abdullah khan <ak786shariq@gmail.com>
2024-04-23 12:19:09 +0530

    Choose us

---
+++ src/ProductsSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductTitle from './ProductTitle'
+import './ProductStyle.css'
+function ProductsSection() {
+    return (
+        <div className="product-area-3 product-4 area-padding">
+            <ProductTitle />
+        </div>
+    )
+}
+
+export default ProductsSection
